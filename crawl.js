577def072ac7f27ada82216a963c1bb85a642aff
Nikita Jajodia <nikitajajodia@admins-MacBook-Pro.local>
2017-06-30 16:05:41 +0530

    add crawler for other wordpress site

---
+++ crawl.js
@@ -0,0 +1,22 @@
+var fs       = require('fs');
+var request  = require('request');
+var cheerio  = require('cheerio');
+var jsonfile = require('jsonfile');
+
+request('dreamysoulsite.wordpress.com', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var title, url, data = [];
+    var $ = cheerio.load(html);
+    $('h1.entry-title').each(function(i, element){
+      title = $(this);
+      url = $(this).find('a');
+      data.push({
+      	'title': title.text(),
+      	'url': url.attr('href')
+      })
+    });
+    jsonfile.writeFile('./data.json', data, {spaces: 2}, function(err) {
+      console.error("error ", err);
+    });
+  }
+});
